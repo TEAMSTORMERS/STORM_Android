772f72618b928eddc5d7daee9a67d55e63d365b8
김성규 <link5658@gmail.com>
2020-07-01 15:15:21 +0900

    add: BaseActivity 추가 및 적용
    앞으로 Activity를 만들면서 공통적으로 발생하는 로직은 BaseActivity로 옮겨 중복을 최소화하면 좋겠습니다.

--- app/src/main/java/com/stormers/storm/MainActivity.kt
+++ app/src/main/java/com/stormers/storm/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.stormers.storm
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.stormers.storm.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
---
+++ app/src/main/java/com/stormers/storm/base/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.stormers.storm.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
