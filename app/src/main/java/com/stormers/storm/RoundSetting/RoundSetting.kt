ff168593358accab7c62de3b9509aaf7af5b563b
SONPYEONGHWA <ssrsph@naver.com>
2020-07-01 23:28:10 +0900

    add:Host Round Setting xml 추가

---
+++ app/src/main/java/com/stormers/storm/RoundSetting/RoundSetting.kt
@@ -0,0 +1,11 @@
+package com.stormers.storm.RoundSetting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RoundSetting : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_round_setting)
+    }
+}
