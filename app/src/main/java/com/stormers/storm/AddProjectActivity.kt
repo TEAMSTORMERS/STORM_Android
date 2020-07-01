7a5d378319b12585f569fb9d7442d285dcb618e3
김성규 <link5658@gmail.com>
2020-07-02 05:11:26 +0900

    add: AddProjectActivity 추가
    레이아웃 조금 더 조정이 필요함.
    스타일로 속성을 묶어야 함.

---
+++ app/src/main/java/com/stormers/storm/AddProjectActivity.kt
@@ -0,0 +1,12 @@
+package com.stormers.storm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.stormers.storm.base.BaseActivity
+
+class AddProjectActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_project)
+    }
+}
