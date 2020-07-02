0ebc77a5c7e95bd549315379ae1938df8ff79113
SONPYEONGHWA <ssrsph@naver.com>
2020-07-03 06:05:55 +0900

    add:mainview toolbar 아이콘추가

--- app/src/main/java/com/stormers/storm/MainActivity.kt
+++ app/src/main/java/com/stormers/storm/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.stormers.storm
 
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
 import com.stormers.storm.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_add_project.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val mainview_toolbar = findViewById(R.id.mainview_toolbar) as Toolbar
+
+        setSupportActionBar(mainview_toolbar)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.mainview_ic_bamburgerbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val ab = supportActionBar!!
+        ab.setDisplayShowTitleEnabled(false)
+
+
     }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
 }
