837905f661f3a858e7495e9735a9268e58bfd247
김성규 <link5658@gmail.com>
2020-07-02 18:44:54 +0900

    feat: 툴바기능, 상태바 색상 변경
    줄 간격 조절, 마이페이지 아이콘 크기 수정 해야함.
    컬러, 백그라운드 xml 중복 해결해야함

--- app/src/main/java/com/stormers/storm/AddProjectActivity.kt
+++ app/src/main/java/com/stormers/storm/AddProjectActivity.kt
@@ -1,12 +1,45 @@
 package com.stormers.storm
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.stormers.storm.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_add_project.*
 
 class AddProjectActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_project)
+
+        //뒤로 가기 버튼 설장
+        setSupportActionBar(toolbar_addproject)
+
+        supportActionBar?.let {
+            it.setDisplayShowTitleEnabled(false)
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.host_a_1_btn_back)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    //메뉴 선택시
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+
+            //Fixme: 마이페이지 아이콘이 작아지는 문제 해결
+            R.id.menu_toolbar_mypage -> {
+                //Todo: 마이페이지로 이동하는 코드
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
