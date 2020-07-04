5c41817ecc40aa2c432a4e279bd9af4538b6e28a
김성규 <link5658@gmail.com>
2020-07-04 16:49:16 +0900

    docs: 주석 추가

--- app/src/main/java/com/stormers/storm/customwidget/StormDialog.kt
+++ app/src/main/java/com/stormers/storm/customwidget/StormDialog.kt
@@ -12,6 +12,17 @@ import com.stormers.storm.R
 import kotlinx.android.synthetic.main.dialog_custom.view.*
 import kotlinx.android.synthetic.main.dialog_custom.view.imageview_dialog_symbol
 
+/**
+ * 간단 다이얼로그 생성 클래스
+ * @param dialogType : 다이얼로그의 타입
+ * @param message : 다이얼로그에 들어갈 문구
+ * @param listener : 확인 버튼을 눌렀을 때 실행될 리스너 (필수 아님)
+ * listener 는 StromDialog.OnClickListener 인터페이스를 구현하여 사용
+ *
+ * @const NORMAL_WITH_ONE_BUTTON : 확인 버튼과 안내 문구 하나만 있는 다이얼로그
+ * @const CODE_FOR_PARTICIPATION : 참여 코드를 보여주는 다이얼로그
+ * @const WAITING_NO_BUTTON : 로딩 중일 때 띄우는 다이얼로그
+ */
 class StormDialog(private val dialogType: Int, val message: String) : DialogFragment() {
 
     companion object {
@@ -38,10 +49,13 @@ class StormDialog(private val dialogType: Int, val message: String) : DialogFrag
             }
 
             CODE_FOR_PARTICIPATION -> {
+                //Todo: 참여 코드 복사 기능 추가
                 view.group_dialog_code.visibility = View.VISIBLE
             }
 
-            else -> {
+            else -> { // WAITING_NO_BUTTON
+                //Todo: 애니메이션이 계속 진행되도록
+                //Todo: 로딩이 끝나면 다이얼로그를 닫을 수 있는 메서드 추가
                 view.imageview_dialog_symbol.setImageResource(R.drawable.h_roundstart_popup_symbol)
                 view.group_dialog_code.visibility = View.GONE
                 view.constraintLayout_dialog_button.visibility = View.GONE
