138090ec03c5bf6e9369afda2c7d74b8ad421ae5
heewon <ymcho24@naver.com>
2020-07-05 04:23:22 +0900

    RoundmeetingFragment 및 Activity 생성

---
+++ app/src/main/java/com/stormers/storm/roundmeeting/recyclerview/RoundmeetingData.kt
@@ -0,0 +1,7 @@
+package com.stormers.storm.roundmeeting.recyclerview
+
+import android.graphics.drawable.Drawable
+
+class RoundmeetingData (
+    val ImageView_added_card_roundmeeting : Drawable?
+)
