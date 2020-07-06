d25e4bbc25d75237d2c5ed08f6d0d1313c0ed292
김성규 <link5658@gmail.com>
2020-07-06 17:03:31 +0900

    refactor: RoundMeetingFragment의 RecyclerView 개선

--- app/src/main/java/com/stormers/storm/roundmeeting/recyclerview/RoundmeetingViewHolder.kt
+++ app/src/main/java/com/stormers/storm/roundmeeting/recyclerview/RoundmeetingViewHolder.kt
@@ -1,18 +1,16 @@
 package com.stormers.storm.roundmeeting.recyclerview
 
-import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.stormers.storm.R
-import com.stormers.storm.addcard.recyclerview.AddedCardData
+import com.stormers.storm.base.BaseViewHolder
 
-class RoundmeetingViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+class RoundmeetingViewHolder(parent: ViewGroup) : BaseViewHolder<RoundmeetingData>(R.layout.item_roundmeeting, parent) {
     val ImageView_added_card_roundmeeting = itemView.findViewById<ImageView>(R.id.ImageView_added_card_roundmeeting)
 
-
-    fun bind(RoundmeetingData : RoundmeetingData){
-        Glide.with(itemView).load(RoundmeetingData.ImageView_added_card_roundmeeting).into(ImageView_added_card_roundmeeting)
+    override fun bind(data : RoundmeetingData){
+        Glide.with(itemView).load(data.ImageView_added_card_roundmeeting).into(ImageView_added_card_roundmeeting)
     }
 
 }
