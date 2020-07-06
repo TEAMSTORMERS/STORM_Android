89c9a8a0a4e9a93607fe1074b8de6502ba09504b
SONPYEONGHWA <ssrsph@naver.com>
2020-07-06 22:10:56 +0900

    fix:round설정완료 프래그먼트

--- app/src/main/java/com/stormers/storm/RoundSetting/HostRoundSettingActivity.kt
+++ app/src/main/java/com/stormers/storm/RoundSetting/HostRoundSettingActivity.kt
@@ -1,7 +1,12 @@
 package com.stormers.storm.RoundSetting
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentContainer
+import androidx.fragment.app.FragmentTransaction
 import com.stormers.storm.R
 import kotlinx.android.synthetic.main.activity_host_round_setting.*
 
@@ -11,6 +16,8 @@ class HostRoundSettingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_host_round_setting)
 
         viewpager_host_round_setting_fragment.adapter = RoundSettingAdapter(supportFragmentManager)
-        viewpager_host_round_setting_fragment.offscreenPageLimit = 0
-    }
+        viewpager_host_round_setting_fragment.offscreenPageLimit = 2
+
+        }
+
 }
--- app/src/main/java/com/stormers/storm/RoundSetting/RoundSettingAdapter.kt
+++ app/src/main/java/com/stormers/storm/RoundSetting/RoundSettingAdapter.kt
@@ -3,13 +3,16 @@ package com.stormers.storm.RoundSetting
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.stormers.storm.RoundStartFragment
 
 class RoundSettingAdapter (fm:FragmentManager):FragmentPagerAdapter(fm,
     BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
     override fun getItem(position: Int): Fragment {
-        return Host_Round_Setting_Fragment()
+        return when(position){
+            0 -> HostRoundSettingFragment()
+            else -> RoundStartFragment()
+        }
     }
-
-    override fun getCount() =1
+    override fun getCount() =2
 }
