4a5a2990c1ce0bd958ddc44dc7b05d2d2dbf8447
김성규 <link5658@gmail.com>
2020-07-08 23:24:01 +0900

    refactor: 의존성 제거 완료

--- app/src/main/java/com/stormers/storm/ui/ScrapCardCollectingActivity.kt
+++ app/src/main/java/com/stormers/storm/ui/ScrapCardCollectingActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.stormers.storm.R
 import com.stormers.storm.card.adapter.CardAdapter
+import com.stormers.storm.card.model.CardModel
+import com.stormers.storm.user.UserModel
 import kotlinx.android.synthetic.main.activity_scrapcard_collecting.*
 
 class ScrapCardCollectingActivity : AppCompatActivity() {
@@ -16,59 +18,64 @@ class ScrapCardCollectingActivity : AppCompatActivity() {
 
         scrapCollectAdapter = CardAdapter()
         RecyclerView_scrap_collect.adapter = scrapCollectAdapter
-        loadScrapCollectDatas()
+        scrapCollectAdapter.addAll(loadCardDataOfRound())
 
     }
 
-    private fun loadScrapCollectDatas() {
+    private fun loadCardDataOfRound(): MutableList<CardModel> {
+        val data = mutableListOf<CardModel>()
+        val gyu = UserModel(
+            "https://avatars2.githubusercontent.com/u/57310034?s=460&u=3b6de8b863bdc2b902bf6cfe080bc8d34e93c348&v=4",
+            "성규"
+        )
+        val piece = UserModel(
+            "https://avatars0.githubusercontent.com/u/56873136?s=460&v=4",
+            "평화"
+        )
+        val one = UserModel(
+            "https://avatars2.githubusercontent.com/u/52772787?s=460&u=4a9f12ef174f88ec143b70f4fcaaa8f1b2d87b43&v=4",
+            "희원"
+        )
 
-        val datas = mutableListOf<ScrapCollectData>()
 
-        datas.apply {
+        data.apply {
             add(
-                ScrapCollectData(
-                    //Todo: 더미데이터 (카드 상세 이미지가 들어갈 공간입니다)
-                    imageView_scrap_collect = baseContext?.getDrawable(R.drawable.storm_logo_example)
+                CardModel(
+                    "https://avatars2.githubusercontent.com/u/67626159?s=400&u=ec57a4e02436867cedb86350cc9e4d33d694b2f4&v=4",
+                    true,
+                    gyu
                 )
             )
             add(
-                ScrapCollectData(
-                    imageView_scrap_collect = baseContext?.getDrawable(R.drawable.storm_logo_example)
+                CardModel(
+                    "https://avatars2.githubusercontent.com/u/67626159?s=400&u=ec57a4e02436867cedb86350cc9e4d33d694b2f4&v=4",
+                    true,
+                    piece
                 )
             )
             add(
-                ScrapCollectData(
-                    imageView_scrap_collect = baseContext?.getDrawable(R.drawable.storm_logo_example)
+                CardModel(
+                    "https://avatars2.githubusercontent.com/u/67626159?s=400&u=ec57a4e02436867cedb86350cc9e4d33d694b2f4&v=4",
+                    true,
+                    one
                 )
             )
             add(
-                ScrapCollectData(
-                    imageView_scrap_collect = baseContext?.getDrawable(R.drawable.storm_logo_example)
+                CardModel(
+                    "https://avatars2.githubusercontent.com/u/67626159?s=400&u=ec57a4e02436867cedb86350cc9e4d33d694b2f4&v=4",
+                    true,
+                    gyu
                 )
             )
             add(
-                ScrapCollectData(
-                    imageView_scrap_collect = baseContext?.getDrawable(R.drawable.storm_logo_example)
+                CardModel(
+                    "https://avatars2.githubusercontent.com/u/67626159?s=400&u=ec57a4e02436867cedb86350cc9e4d33d694b2f4&v=4",
+                    true,
+                    piece
                 )
             )
-            add(
-                ScrapCollectData(
-                    imageView_scrap_collect = baseContext?.getDrawable(R.drawable.storm_logo_example)
-                )
-            )
-            add(
-                ScrapCollectData(
-                    imageView_scrap_collect = baseContext?.getDrawable(R.drawable.storm_logo_example)
-                )
-            )
-            add(
-                ScrapCollectData(
-                    imageView_scrap_collect = baseContext?.getDrawable(R.drawable.storm_logo_example)
-                )
-            )
-
         }
 
-        scrapCollectAdapter.addAll(datas)
+        return data
     }
 }
