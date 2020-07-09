92394b9d603e061a4758f7abd817efb8f0c9c1c0
heewon <ymcho24@naver.com>
2020-07-10 08:47:28 +0900

    멤버 프로젝트 시작 대기

--- app/src/main/java/com/stormers/storm/project/fragment/WaitingForStartingProjectFragment.kt
+++ app/src/main/java/com/stormers/storm/project/fragment/WaitingForStartingProjectFragment.kt
@@ -1,16 +1,25 @@
 package com.stormers.storm.project.fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.stormers.storm.R
+import com.stormers.storm.customview.dialog.StormDialog
+import com.stormers.storm.customview.dialog.StormDialogBuilder
+import com.stormers.storm.customview.dialog.StormDialogButton
 import com.stormers.storm.user.ParticipantAdapter
 import com.stormers.storm.user.UserModel
 import com.stormers.storm.util.MarginDecoration
+import kotlinx.android.synthetic.main.fragment_host_round_setting.*
+import kotlinx.android.synthetic.main.fragment_waiting_for_starting_project.*
 import kotlinx.android.synthetic.main.fragment_waiting_for_starting_project.view.*
 import kotlinx.android.synthetic.main.layout_list_of_participant.view.*
 
@@ -19,7 +28,9 @@ class WaitingForStartingProjectFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.fragment_waiting_for_starting_project, container, false)
+
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -31,7 +42,42 @@ class WaitingForStartingProjectFragment : Fragment() {
         }
 
         participantAdapter.addAll(loadData())
+
+        cardview_waitingproject_checkrules.setOnClickListener{
+
+            //룰 리마인더 다이얼로그
+            val buttonArray = ArrayList<StormDialogButton>()
+
+            buttonArray.add(
+                StormDialogButton(
+                    "확인",
+                    object :
+                        StormDialogButton.OnClickListener {
+                        override fun onClick() {
+                            //Toast.makeText(context, "확인 눌렀음", Toast.LENGTH_SHORT).show()
+                            imageview_checkrules_uncheckcircle.visibility = View.INVISIBLE
+                            imageview_checkrules_checkcircle.visibility = View.VISIBLE
+                        }
+                    })
+            )
+
+            getFragmentManager()?.let { it1 ->
+                StormDialogBuilder(
+                    R.drawable.m_brainstormingreminder_popup_lightning,
+                    "브레인스토밍 룰 리마인더"
+                )
+                    .setContentRes(R.layout.view_rule_reminder)
+                    .setButtonArray(buttonArray)
+                    .build()
+                    .show(it1, StormDialog.TAG)
+            }
+
+        }
+
+
+
     }
+
 
 
     private fun loadData(): MutableList<UserModel> {
