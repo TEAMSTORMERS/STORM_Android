05d73837fd20e305534d91f5313607f258e9c679
김성규 <link5658@gmail.com>
2020-07-09 14:43:06 +0900

    update: 로고 쉽게 넣을 수 있도록 수정

--- app/src/main/java/com/stormers/storm/customview/dialog/StormDialogBuilder.kt
+++ app/src/main/java/com/stormers/storm/customview/dialog/StormDialogBuilder.kt
@@ -9,22 +9,18 @@ import com.stormers.storm.R
  * StormDialog를 생성하기 위한 BuilderClass
  * @since 2020.07.05
  * @author KIM SEOUNGGYU
+ *
+ *  @param dialogImg 다이얼로그에 표시될 이미지
+ * @param title 표시될 문구
  */
-class StormDialogBuilder {
+class StormDialogBuilder(private var dialogImg: Int, private var title: String) {
 
     //상수 모음
-    //자주 만든다 싶은 다이얼로그는 여기에 상수로 등록하고 자동 생성 로직 추가
     companion object {
-        const val CUSTOMIZING = 0
-        const val CODE_FOR_PARTICIPATION = 1
+        const val STORM_LOGO = 0
+        const val THUNDER_LOGO = 1
+        const val LOADING_LOGO = 2
     }
-
-    private var dialogType: Int
-
-    @DrawableRes
-    private var imageRes: Int
-
-    private var title: String
 
     private var contentText: String? = null
 
@@ -35,53 +31,16 @@ class StormDialogBuilder {
 
     private var horizontalButton: ArrayList<StormDialogButton>? = null
 
-    //빌드 타입이 정의된 경우 자동 생성
-    constructor(dialogType: Int) {
-        this.dialogType = dialogType
-
-        when (dialogType) {
-            CODE_FOR_PARTICIPATION -> {
-                imageRes = R.drawable.h_projectsetting_popup_lightning
-                title = "참여 코드 생성 완료!"
-                contentRes = R.layout.view_participation_code
-
-                //Todo: 참여코드 입력 파라미터 추가
-                //Todo: 참여코드 복사 로직 추가
-
-                val buttons = ArrayList<StormDialogButton>()
-
-                buttons.add(
-                    StormDialogButton(
-                        "확인", true,
-                        object :
-                            StormDialogButton.OnClickListener {
-                            override fun onClick() {
-                                //nothing to do
-                            }
-                        })
-                )
-
-                this.buttonArray = buttons
-            }
-
-            else -> {
-                imageRes = -1
-                title = ""
-            }
-        }
-    }
-
-    /**
-     * 커스텀 모드 필수 파라미터
-     * @param imageRes 다이얼로그에 표시될 이미지
-     * @param title 표시될 문구
-     */
-    constructor(@DrawableRes imageRes: Int, title: String) : this(CUSTOMIZING) {
-        this.imageRes = imageRes
-        this.title = title
-    }
-
     fun build() : StormDialog {
+        val imageRes = when(dialogImg) {
+
+            STORM_LOGO -> R.drawable.h_roundstart_popup_symbol
+            THUNDER_LOGO -> R.drawable.h_projectsetting_popup_lightning
+            LOADING_LOGO -> -1 //Todo: 애니메이션 로고 넣기
+
+            else -> throw IllegalArgumentException("Unresolved dialog image")
+        }
+
         return StormDialog(
             imageRes,
             title,
