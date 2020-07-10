854c6dbef880eb633afff5d0a092e80b75875ccd
김성규 <link5658@gmail.com>
2020-07-10 23:34:25 +0900

    feat: 그림 지우기 기능 추가

--- app/src/main/java/com/stormers/storm/canvas/fragment/CanvasDrawingFragment.kt
+++ app/src/main/java/com/stormers/storm/canvas/fragment/CanvasDrawingFragment.kt
@@ -7,19 +7,32 @@ import android.view.View
 import com.stormers.storm.R
 import com.stormers.storm.canvas.base.BaseCanvasFragment
 import kotlinx.android.synthetic.main.fragment_round_canvas.*
+import kotlinx.android.synthetic.main.view_signaturepad.*
 
 class CanvasDrawingFragment : BaseCanvasFragment(DRAWING_MODE) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        //뷰 초기화
+        initView()
+        //그림 그릴 수 있도록 초기화
+        initDrawCanvas()
+    }
+
+    private fun initView() {
         imagebutton_change_text.alpha = 0.5f
 
         imagebutton_change_text.setOnClickListener {
             showChangeDialog()
         }
+    }
 
+    private fun initDrawCanvas() {
         LayoutInflater.from(context).inflate(R.layout.view_signaturepad, cardview_roundcanvas_canvas)
-        
+
+        imagebutton_canvas_trash.setOnClickListener{
+            signaturepad.clear()
+        }
     }
 }
