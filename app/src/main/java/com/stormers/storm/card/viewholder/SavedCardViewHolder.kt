807dd29d2135393bb82ed57bff522f28a7245fed
김성규 <link5658@gmail.com>
2020-07-12 17:48:08 +0900

    update: DB에 저장된 TextCard를 화면에 표시하도록 수정

--- app/src/main/java/com/stormers/storm/card/viewholder/SavedCardViewHolder.kt
+++ app/src/main/java/com/stormers/storm/card/viewholder/SavedCardViewHolder.kt
@@ -1,6 +1,5 @@
 package com.stormers.storm.card.viewholder
 
-import android.graphics.Bitmap
 import android.view.ViewGroup
 import com.stormers.storm.R
 import com.stormers.storm.base.BaseViewHolder
@@ -10,15 +9,20 @@ import kotlinx.android.synthetic.main.item_heart_card.view.*
 
 class SavedCardViewHolder(parent: ViewGroup) : BaseViewHolder<SavedCardEntity>(R.layout.item_heart_card, parent) {
     override fun bind(data: SavedCardEntity) {
+
+        //카드 타입이 그림인 경우
         if (data.cardType == SavedCardEntity.DRAWING) {
             val bitmap = BitmapConverter.stringToBitmap(data.content)
 
             bitmap?.let {
-                itemView.stormcard_itemheart.setBitmap(bitmap)
+                itemView.stormcard_itemheart.setBitmap(it)
             }
+
+        //카드 타입이 텍스트인 경우
         } else {
-            //텍스트 형식 추가
-            //itemView.stormcard_itemheart.setText(data.content)
+            data.content?.let {
+                itemView.stormcard_itemheart.setText(it)
+            }
         }
     }
 }
