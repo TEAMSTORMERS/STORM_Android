407247d44b2ddacb579f429f8825a9d6bf0f8e19
김성규 <link5658@gmail.com>
2020-07-12 17:03:00 +0900

    feat: 텍스트로 카드를 추가하는 기능

--- app/src/main/java/com/stormers/storm/canvas/fragment/CanvasTextFragment.kt
+++ app/src/main/java/com/stormers/storm/canvas/fragment/CanvasTextFragment.kt
@@ -1,9 +1,14 @@
 package com.stormers.storm.canvas.fragment
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.Toast
+import com.stormers.storm.R
+import com.stormers.storm.RoundSetting.AddCardFragment
 import com.stormers.storm.canvas.base.BaseCanvasFragment
 import kotlinx.android.synthetic.main.fragment_round_canvas.*
+import kotlinx.android.synthetic.main.view_addcard_edittext.*
 
 class CanvasTextFragment : BaseCanvasFragment(TEXT_MODE) {
 
@@ -14,6 +19,26 @@ class CanvasTextFragment : BaseCanvasFragment(TEXT_MODE) {
 
         imagebutton_change_draw.setOnClickListener {
             showChangeDialog()
+        }
+
+        LayoutInflater.from(context).inflate(R.layout.view_addcard_edittext, cardview_roundcanvas_canvas)
+
+        imagebutton_canvas_trash.setOnClickListener {
+            edittext_addcard.text = null
+
+            Toast.makeText(context, "지웠습니다!", Toast.LENGTH_SHORT).show()
+        }
+
+        stormbutton_roundcanvas_apply.setOnClickListener {
+
+            if (!edittext_addcard.text.isNullOrBlank()) {
+                val cardText = edittext_addcard.text.toString()
+                //Todo: cardText 서버로 전송
+                Toast.makeText(context, "저장되었습니다.", Toast.LENGTH_SHORT).show()
+                goToFragment(AddCardFragment::class.java, null)
+            } else {
+                Toast.makeText(context, "카드가 비어있습니다!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
