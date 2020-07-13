85a16c3134de8218e325e27a532103e995b98a38
김성규 <link5658@gmail.com>
2020-07-14 03:30:42 +0900

    refactor: ExpandCardAdapter, ViewHolder 생성 및 Item레이아웃 수정

---
+++ app/src/main/java/com/stormers/storm/card/viewholder/ExpandCardViewHolder.kt
@@ -0,0 +1,37 @@
+package com.stormers.storm.card.viewholder
+
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.OvalShape
+import android.view.View
+import android.view.ViewGroup
+import com.stormers.storm.R
+import com.stormers.storm.base.BaseViewHolder
+import com.stormers.storm.card.model.SavedCardEntity
+import com.stormers.storm.card.util.BitmapConverter
+import kotlinx.android.synthetic.main.item_expandcard_card.view.*
+
+class ExpandCardViewHolder(parent: ViewGroup) : BaseViewHolder<SavedCardEntity>(R.layout.item_expandcard_card, parent) {
+
+    override fun bind(data: SavedCardEntity) {
+
+        //배경 둥글게 자르기
+        itemView.imageview_expandcard_profile.run {
+            background = ShapeDrawable(OvalShape())
+            clipToOutline = true
+        }
+
+        if (data.isScraped == SavedCardEntity.TRUE) {
+            itemView.imagebutton_expandcard_heart.setImageResource(R.drawable.scrapcard_btn_heart_1)
+        } else {
+            itemView.imagebutton_expandcard_heart.setImageResource(R.drawable.h_roundmeeting_btn_heart4)
+        }
+
+        if (data.cardType == SavedCardEntity.DRAWING) {
+            itemView.imageview_expandcard_content.setImageBitmap(BitmapConverter.stringToBitmap(data.content))
+            itemView.textview_expandcard_content.visibility = View.INVISIBLE
+        } else {
+            itemView.textview_expandcard_content.text = data.content
+            itemView.imageview_expandcard_content.visibility = View.INVISIBLE
+        }
+    }
+}
--- app/src/main/java/com/stormers/storm/ui/RoundMeetingExpandActivity.kt
+++ app/src/main/java/com/stormers/storm/ui/RoundMeetingExpandActivity.kt
@@ -1,39 +1,20 @@
 package com.stormers.storm.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.viewpager.widget.ViewPager
 import com.stormers.storm.R
+import com.stormers.storm.base.BaseActivity
 import com.stormers.storm.card.adapter.ExpandCardAdapter
-import com.stormers.storm.card.model.CardModel
 import kotlinx.android.synthetic.main.activity_round_meeting_expand.*
 
-class RoundMeetingExpandActivity : AppCompatActivity() {
+class RoundMeetingExpandActivity : BaseActivity() {
 
-  val adapter = ExpandCardAdapter(expandCardList)
-
-
+    private val expandCardAdapter: ExpandCardAdapter by lazy { ExpandCardAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_round_meeting_expand)
-        viewpager_fragment_card_expand.adapter = adapter
+        viewpager_fragment_card_expand.adapter = expandCardAdapter
 
 
-    }
-    companion object{
-        val expandCardList = arrayListOf(
-            CardModel("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRdXp-MREaa6k7N1cD1UjvDLnvmb_iGS6qioQ&usqp=CAU",true,null),
-            CardModel("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRdXp-MREaa6k7N1cD1UjvDLnvmb_iGS6qioQ&usqp=CAU",true,null),
-            CardModel("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRdXp-MREaa6k7N1cD1UjvDLnvmb_iGS6qioQ&usqp=CAU",true,null),
-            CardModel("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRdXp-MREaa6k7N1cD1UjvDLnvmb_iGS6qioQ&usqp=CAU",true,null),
-            CardModel("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRdXp-MREaa6k7N1cD1UjvDLnvmb_iGS6qioQ&usqp=CAU",true,null),
-            CardModel("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRdXp-MREaa6k7N1cD1UjvDLnvmb_iGS6qioQ&usqp=CAU",true,null),
-            CardModel("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRdXp-MREaa6k7N1cD1UjvDLnvmb_iGS6qioQ&usqp=CAU",true,null),
-            CardModel("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRdXp-MREaa6k7N1cD1UjvDLnvmb_iGS6qioQ&usqp=CAU",true,null),
-            CardModel("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRdXp-MREaa6k7N1cD1UjvDLnvmb_iGS6qioQ&usqp=CAU",true,null),
-            CardModel("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRdXp-MREaa6k7N1cD1UjvDLnvmb_iGS6qioQ&usqp=CAU",true,null)
-
-            )
     }
 }
