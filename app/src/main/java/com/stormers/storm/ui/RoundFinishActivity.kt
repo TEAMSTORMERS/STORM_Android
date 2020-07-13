07391a2fbb90784139e2a11bb4970ef2303d2dcc
heewon <ymcho24@naver.com>
2020-07-14 02:35:36 +0900

    finish 버튼 클릭 시 dialog 생성

---
+++ app/src/main/java/com/stormers/storm/ui/RoundFinishActivity.kt
@@ -0,0 +1,58 @@
+package com.stormers.storm.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.stormers.storm.R
+import com.stormers.storm.base.BaseActivity
+import com.stormers.storm.card.fragment.RoundmeetingFragment
+import com.stormers.storm.customview.dialog.StormDialogBuilder
+import com.stormers.storm.customview.dialog.StormDialogButton
+import kotlinx.android.synthetic.main.activity_round_progress.*
+import kotlinx.android.synthetic.main.fragment_waiting_for_starting_project.*
+
+class RoundFinishActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_round_progress)
+        goToFragment(RoundmeetingFragment::class.java, null)
+
+        button_scrapcard_save_roundmeeting.visibility = View.VISIBLE
+
+        button_scrapcard_save_roundmeeting.setOnClickListener{
+            val buttonArray = ArrayList<StormDialogButton>()
+
+            buttonArray.add(
+                StormDialogButton("다음 ROUND 진행", true, object : StormDialogButton.OnClickListener {
+                    override fun onClick() {
+                        Toast.makeText(applicationContext, "확인 눌렀음", Toast.LENGTH_SHORT).show()
+
+                    }
+                })
+            )
+            buttonArray.add(
+                StormDialogButton("프로젝트 종료 후 최종 정리", true, object : StormDialogButton.OnClickListener {
+                    override fun onClick() {
+                        val intent = Intent(this@RoundFinishActivity, ParticipatedProjectDetailActivity::class.java)
+                        startActivity(intent)
+                    }
+                })
+            )
+
+            StormDialogBuilder(StormDialogBuilder.THUNDER_LOGO, "ROUND 1 종료")
+                .setButtonArray(buttonArray)
+                .build()
+                .show(supportFragmentManager, "roundfinish")
+
+        }
+
+    }
+
+    override fun initFragmentId(): Int? {
+        return R.id.framelayout_roundprogress_fragment
+    }
+
+
+}
--- app/src/main/java/com/stormers/storm/ui/RoundMeetingExpandActivity.kt
+++ app/src/main/java/com/stormers/storm/ui/RoundMeetingExpandActivity.kt
@@ -10,18 +10,18 @@ import kotlinx.android.synthetic.main.activity_round_meeting_expand.*
 
 class RoundMeetingExpandActivity : AppCompatActivity() {
 
-  val adapter = ExpandCardAdapter(expandCardList)
+  //val adapter = ExpandCardAdapter(expandCardList)
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_round_meeting_expand)
-        viewpager_fragment_card_expand.adapter = adapter
+        //viewpager_fragment_card_expand.adapter = adapter
 
 
     }
-    companion object{
+    /*companion object{
         val expandCardList = arrayListOf(
             CardModel("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRdXp-MREaa6k7N1cD1UjvDLnvmb_iGS6qioQ&usqp=CAU",true,null),
             CardModel("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRdXp-MREaa6k7N1cD1UjvDLnvmb_iGS6qioQ&usqp=CAU",true,null),
@@ -35,5 +35,5 @@ class RoundMeetingExpandActivity : AppCompatActivity() {
             CardModel("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRdXp-MREaa6k7N1cD1UjvDLnvmb_iGS6qioQ&usqp=CAU",true,null)
 
             )
-    }
+    }*/
 }
