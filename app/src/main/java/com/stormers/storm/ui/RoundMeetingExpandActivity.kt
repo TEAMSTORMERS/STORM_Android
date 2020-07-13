97dfb7255dc0075cd2685c34ddb52e01a711928b
김성규 <link5658@gmail.com>
2020-07-14 03:41:47 +0900

    update: expand card 레이아웃 수정

--- app/src/main/java/com/stormers/storm/ui/RoundMeetingExpandActivity.kt
+++ app/src/main/java/com/stormers/storm/ui/RoundMeetingExpandActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import com.stormers.storm.R
 import com.stormers.storm.base.BaseActivity
 import com.stormers.storm.card.adapter.ExpandCardAdapter
-import kotlinx.android.synthetic.main.activity_round_meeting_expand.*
+import kotlinx.android.synthetic.main.activity_expandcard.*
 
 class RoundMeetingExpandActivity : BaseActivity() {
 
@@ -12,7 +12,7 @@ class RoundMeetingExpandActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_round_meeting_expand)
+        setContentView(R.layout.activity_expandcard)
         viewpager_fragment_card_expand.adapter = expandCardAdapter
 
 
