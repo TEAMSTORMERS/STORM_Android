b82054c189797dea797922fa955e5ebd90c01971
김성규 <link5658@gmail.com>
2020-07-14 05:04:57 +0900

    update: 애니메이션...지정

--- app/src/main/java/com/stormers/storm/ui/RoundMeetingExpandActivity.kt
+++ app/src/main/java/com/stormers/storm/ui/RoundMeetingExpandActivity.kt
@@ -4,17 +4,25 @@ import android.os.Bundle
 import com.stormers.storm.R
 import com.stormers.storm.base.BaseActivity
 import com.stormers.storm.card.adapter.ExpandCardAdapter
+import com.stormers.storm.card.repository.SavedCardRepository
+import com.stormers.storm.util.DepthPageTransformer
 import kotlinx.android.synthetic.main.activity_expandcard.*
 
 class RoundMeetingExpandActivity : BaseActivity() {
 
     private val expandCardAdapter: ExpandCardAdapter by lazy { ExpandCardAdapter() }
 
+    private val savedCardRepository: SavedCardRepository by lazy { SavedCardRepository(application) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_expandcard)
-        viewpager_fragment_card_expand.adapter = expandCardAdapter
+        viewpager_fragment_card_expand.run {
+            adapter = expandCardAdapter
+            offscreenPageLimit = 3
+            setPageTransformer(DepthPageTransformer())
+        }
 
-
+        expandCardAdapter.addAll(savedCardRepository.getAll(1, 1))
     }
 }
