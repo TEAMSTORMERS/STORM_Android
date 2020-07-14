b93dde7f0b3b9f37db1cd50fa042b16d4db97338
SONPYEONGHWA <ssrsph@naver.com>
2020-07-14 20:44:01 +0900

    add: AddProjectActivity.kt 프로젝트 추가 서버통신 구현

--- app/src/main/java/com/stormers/storm/network/InterfaceAddProject.kt
+++ app/src/main/java/com/stormers/storm/network/InterfaceAddProject.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface InterfaceAddProject {
-    @POST
+    @POST("/project")
     fun addProject(@Body body:AddProjectModel) :Call<ResponseAddProject>
 
 }
