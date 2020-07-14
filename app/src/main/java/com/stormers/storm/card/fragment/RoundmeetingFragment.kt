a5512b4f8d8f07006e3d2604cd499d105587dada
heewon <ymcho24@naver.com>
2020-07-14 20:00:00 +0900

    RoundmeetingFragment 수정

--- app/src/main/java/com/stormers/storm/card/fragment/RoundmeetingFragment.kt
+++ app/src/main/java/com/stormers/storm/card/fragment/RoundmeetingFragment.kt
@@ -1,5 +1,6 @@
 package com.stormers.storm.card.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,8 @@ import com.stormers.storm.card.adapter.CardAdapter
 import com.stormers.storm.card.adapter.SavedCardAdapter
 import com.stormers.storm.card.model.CardModel
 import com.stormers.storm.card.repository.SavedCardRepository
+import com.stormers.storm.ui.RoundMeetingExpandActivity
+import com.stormers.storm.ui.RoundProgressActivity
 import com.stormers.storm.user.UserModel
 import kotlinx.android.synthetic.main.fragment_roundmeeting.*
 import kotlin.math.round
@@ -29,7 +32,15 @@ class RoundmeetingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        roundmeetingAdapter = SavedCardAdapter(true, null)
+        roundmeetingAdapter = SavedCardAdapter(true, object: SavedCardAdapter.OnCardClickListener {
+            override fun onCardClick(projectIdx: Int, roundIdx: Int, cardId: Int) {
+                val intent = Intent(context, RoundMeetingExpandActivity::class.java)
+                intent.putExtra("projectIdx", projectIdx)
+                intent.putExtra("roundIdx", roundIdx)
+                intent.putExtra("cardId", cardId)
+                startActivity(intent)
+            }
+        })
         RecyclerView_added_card_roundmeeting.adapter = roundmeetingAdapter
         val data = savedCardRepository.getAll(1, 1)
         //cardAdapter.clear()
