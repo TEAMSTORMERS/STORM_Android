43112a10f64b21d67f77c64cc2ded3b868c69a0a
김성규 <link5658@gmail.com>
2020-07-15 00:36:11 +0900

    refactor: RoundStartFragment의 로직 분리

--- app/src/main/java/com/stormers/storm/round/fragment/RoundStartFragment.kt
+++ app/src/main/java/com/stormers/storm/round/fragment/RoundStartFragment.kt
@@ -1,6 +1,8 @@
 package com.stormers.storm.round.fragment
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,25 +12,38 @@ import androidx.recyclerview.widget.RecyclerView
 import com.stormers.storm.user.ParticipantAdapter
 import com.stormers.storm.user.UserModel
 import com.stormers.storm.R
+import com.stormers.storm.base.BaseFragment
+import com.stormers.storm.customview.StormButton
+import com.stormers.storm.customview.dialog.StormDialog
+import com.stormers.storm.customview.dialog.StormDialogBuilder
+import com.stormers.storm.ui.HostRoundWaitingActivity
+import com.stormers.storm.ui.RoundProgressActivity
 import com.stormers.storm.util.MarginDecoration
+import kotlinx.android.synthetic.main.activity_host_round_setting.*
+import kotlinx.android.synthetic.main.fragment_host_round_setting.*
 import kotlinx.android.synthetic.main.fragment_round_start.view.*
 import kotlinx.android.synthetic.main.layout_list_of_participant.view.*
 
 
-class RoundStartFragment : Fragment() {
+class RoundStartFragment : BaseFragment(R.layout.fragment_round_start) {
 
     private val participantAdapter: ParticipantAdapter by lazy { ParticipantAdapter() }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_round_start, container, false)
-    }
+    private lateinit var activityButton: StormButton
+
+    private lateinit var dialog: StormDialog
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        dialog = StormDialogBuilder(StormDialogBuilder.LOADING_LOGO, "5초 후 라운드가 시작합니다").build()
+
+        initActivityButton()
+
+        initParticipant(view)
+    }
+
+    private fun initParticipant(view: View) {
         view.include_roundstart_participant.recyclerview_participant.run {
             layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
             addItemDecoration(MarginDecoration(context, 15, RecyclerView.VERTICAL))
@@ -38,7 +53,22 @@ class RoundStartFragment : Fragment() {
         participantAdapter.addAll(loadDatas())
     }
 
-    fun loadDatas() : MutableList<UserModel> {
+    private fun initActivityButton() {
+        activityButton = (activity as HostRoundWaitingActivity).stormButton_ok_host_round_setting
+
+        activityButton.setOnClickListener {
+            fragmentManager?.let { it1 -> dialog.show(it1, "round_start") }
+
+            val handler = Handler()
+            val handlerTask = Runnable {
+                startActivity(Intent(activity, RoundProgressActivity::class.java))
+            }
+
+            handler.postDelayed(handlerTask, 5000)
+        }
+    }
+
+    fun loadDatas(): MutableList<UserModel> {
         val datas = mutableListOf<UserModel>()
 
         datas.add(
