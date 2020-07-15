1739f08c8157e5456b2a989e61fb01c9fdd847f8
SONPYEONGHWA <ssrsph@naver.com>
2020-07-15 22:28:35 +0900

    fix:Round Count Retrofit(GET) 서버통신 구현

---
+++ app/src/main/java/com/stormers/storm/project/network/InterfaceRoundCount.kt
@@ -0,0 +1,11 @@
+package com.stormers.storm.project.network
+
+import com.stormers.storm.round.model.ResponseRoundCountModel
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface InterfaceRoundCount {
+    @GET("/round/count/{project_idx}")
+    fun responseRoundCount(@Path("project_idx") projectIdx :Int): Call<ResponseRoundCountModel>
+}
