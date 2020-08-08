952178a0eaf5f5992827bee9dbad3dcff6241387
SONPYEONGHWA <ssrsph@naver.com>
2020-08-08 15:05:27 +0900

    feat:회원가입 xml 작성

---
+++ app/src/main/java/com/stormers/storm/ui/SiginUpActivity.kt
@@ -0,0 +1,11 @@
+package com.stormers.storm.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SiginUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sigin_up)
+    }
+}
