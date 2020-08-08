6b960abfbcc7efd1084e90b8898c2e0b33d4d5ea
SONPYEONGHWA <ssrsph@naver.com>
2020-08-08 15:06:24 +0900

    feat:회원가입xml작성

--- app/src/main/java/com/stormers/storm/ui/SiginUpActivity.kt
+++ app/src/main/java/com/stormers/storm/ui/SiginUpActivity.kt
@@ -1,11 +1,27 @@
 package com.stormers.storm.ui
 
+import android.graphics.Color
+import android.graphics.ColorFilter
+import android.graphics.PorterDuff
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.OvalShape
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract
+import com.stormers.storm.R
+import kotlinx.android.synthetic.main.activity_sigin_up.*
 
 class SiginUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sigin_up)
+
+        setProfileColor()
+
+    }
+
+    fun setProfileColor(){
+        imageview_select_profile_yellow.setBackgroundColor(Color.parseColor("#f5c245"))
     }
 }
