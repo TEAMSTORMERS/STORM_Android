370124899d8decc952ed96ed017b1bddacae28ea
SONPYEONGHWA <ssrsph@naver.com>
2020-08-13 00:01:52 +0900

    add:drawable추가, xml수정

--- app/src/main/java/com/stormers/storm/ui/CompleteSignUpActivity.kt
+++ app/src/main/java/com/stormers/storm/ui/CompleteSignUpActivity.kt
@@ -10,7 +10,7 @@ class CompleteSignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_complete_sign_up)
-        
+
         goToLogInActivity()
     }
 
