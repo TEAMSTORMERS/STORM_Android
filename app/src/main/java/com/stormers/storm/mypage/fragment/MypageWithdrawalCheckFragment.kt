4e1d737faa63dacdce427e9360d30e8312885352
heewon <ymcho24@naver.com>
2020-08-12 18:01:15 +0900

    회원 탈퇴 뷰 완성

---
+++ app/src/main/java/com/stormers/storm/mypage/fragment/MypageWithdrawalCheckFragment.kt
@@ -0,0 +1,14 @@
+package com.stormers.storm.mypage.fragment
+
+import android.os.Bundle
+import android.view.View
+import com.stormers.storm.R
+import com.stormers.storm.base.BaseFragment
+
+class MypageWithdrawalCheckFragment : BaseFragment(R.layout.fragment_mypage_withdrawal_check) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/stormers/storm/mypage/fragment/MypageWithdrawalFragment.kt
@@ -0,0 +1,14 @@
+package com.stormers.storm.mypage.fragment
+
+import android.os.Bundle
+import android.view.View
+import com.stormers.storm.R
+import com.stormers.storm.base.BaseFragment
+
+class MypageWithdrawalFragment : BaseFragment(R.layout.fragment_mypage_withdrawal) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
