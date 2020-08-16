d054d0466681829f94a8340c2c5f8cc01a1d7c6e
김성규 <link5658@gmail.com>
2020-08-17 01:58:36 +0900

    feat: Multiline 커버

--- app/src/main/java/com/stormers/storm/customview/StormEditText.kt
+++ app/src/main/java/com/stormers/storm/customview/StormEditText.kt
@@ -3,12 +3,17 @@ package com.stormers.storm.customview
 import android.content.Context
 import android.content.res.TypedArray
 import android.text.Editable
+import android.text.InputType
 import android.text.TextWatcher
 import android.util.AttributeSet
+import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
-import androidx.appcompat.content.res.AppCompatResources
+import android.widget.LinearLayout
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.view.marginTop
 import com.stormers.storm.R
 import com.stormers.storm.util.MetricsUtil
 import kotlinx.android.synthetic.main.view_edittext_custom.view.*
@@ -84,20 +89,41 @@ class StormEditText : ConstraintLayout {
             edittext_customedittext.hint = null
         }
 
+        //최대 라인 수
         val lines = typedArray.getInteger(R.styleable.StormEditText_android_lines, -1)
 
         if (lines != -1) {
             edittext_customedittext.setLines(lines)
         }
 
-        val inputType = typedArray.getInt(R.styleable.StormEditText_android_inputType, -1)
+        //입력 타입
+        val inputType = typedArray.getInteger(R.styleable.StormEditText_android_inputType, -1)
 
         if (inputType != -1) {
              edittext_customedittext.inputType = inputType
+
+            if (inputType == InputType.TYPE_TEXT_FLAG_MULTI_LINE or InputType.TYPE_CLASS_TEXT) {
+                edittext_customedittext.run {
+                    gravity = Gravity.TOP or Gravity.START
+                    id = View.generateViewId()
+                }
+
+                imageview_customedittext_left.id = View.generateViewId()
+                imagebutton_customedittext.id = View.generateViewId()
+                (edittext_customedittext.layoutParams as LayoutParams).run {
+                    height = 0
+                    setMargins(MetricsUtil.convertDpToPixel(10f, context).toInt(), MetricsUtil.convertDpToPixel(10f, context).toInt(),
+                        MetricsUtil.convertDpToPixel(10f, context).toInt(), MetricsUtil.convertDpToPixel(10f, context).toInt())
+                }
+
+                val constraints = ConstraintSet()
+                constraints.clone(constraintlayout_stormedittext_root)
+                constraints.constrainDefaultHeight(edittext_customedittext.id, ConstraintSet.MATCH_CONSTRAINT_SPREAD)
+                constraints.applyTo(constraintlayout_stormedittext_root)
+            }
         }
 
         val srcDrawable = typedArray.getDrawable(R.styleable.StormEditText_android_drawableStart)
-
 
         if (srcDrawable != null) {
             imageview_customedittext_left.run {
@@ -115,5 +141,9 @@ class StormEditText : ConstraintLayout {
 
     fun removeTextChangedListener(watcher: TextWatcher) {
         this.edittext_customedittext.removeTextChangedListener(watcher)
+    }
+
+    companion object {
+        private const val TAG = "StormEditText"
     }
 }
