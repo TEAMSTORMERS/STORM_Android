c83657d9357b7d36dcc014cc9e443fb30f3bc6b0
heewon <ymcho24@naver.com>
2020-08-19 09:49:09 +0900

    fix: 스크랩한 카드 상세보기 activity 분기

--- app/src/main/java/com/stormers/storm/ui/RoundListActivity.kt
+++ app/src/main/java/com/stormers/storm/ui/RoundListActivity.kt
@@ -44,7 +44,7 @@ class RoundListActivity : AppCompatActivity() {
 
         cardAdapter = SavedCardAdapter(true, object : SavedCardAdapter.OnCardClickListener {
             override fun onCardClick(projectIdx: Int, roundIdx: Int, cardId: Int) {
-                val intent = Intent(this@RoundListActivity, ScrapedCardDetailActivity::class.java)
+                val intent = Intent(this@RoundListActivity, ParticipatedCardDetailActivity::class.java)
                 intent.putExtra("projectIdx", projectIdx)
                 intent.putExtra("roundIdx", roundIdx)
                 intent.putExtra("cardId", cardId)
