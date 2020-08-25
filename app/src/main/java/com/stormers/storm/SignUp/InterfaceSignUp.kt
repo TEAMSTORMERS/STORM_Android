e2c7a1cd9c3379018e0294cd9892ef6ceb50a5d5
SONPYEONGHWA <ssrsph@naver.com>
2020-08-25 18:29:40 +0900

    add:이메일 중복 체크 서버통신 추가

--- app/src/main/java/com/stormers/storm/SignUp/InterfaceSignUp.kt
+++ app/src/main/java/com/stormers/storm/SignUp/InterfaceSignUp.kt
@@ -1,8 +1,10 @@
 package com.stormers.storm.SignUp
 
+import com.stormers.storm.network.SimpleResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
@@ -15,4 +17,8 @@ interface InterfaceSignUp{
         @Part("user_email") userEmail: RequestBody, @Part("user_password") userPassword: RequestBody,
         @Part("user_img_flag") UserImageFlag: RequestBody
     ) : Call<ResponseSignUpModel>
+
+
+    @POST("/user/checkemail")
+    fun requestCheckEmailDuplication(@Body body : CheckEmailDuplicationModel) : Call<SimpleResponse>
 }
