733024f322d7448b2953258c3939593d56adcdf9
heewon <ymcho24@naver.com>
2020-08-25 12:04:55 +0900

    fix: profile 이미지 설정 관련 버그 수정

---
+++ app/src/main/java/com/stormers/storm/util/ProfileCompanion.kt
@@ -0,0 +1,18 @@
+package com.stormers.storm.util
+
+class ProfileCompanion {
+    companion object {
+        //image flag를 나타내는 상수
+        const val USER_IMAGE = 1
+        const val USER_DEFAULT_IMAGE = 0
+
+        //갤러리 접근 권한 관련 상수
+        const val FLAG_REQ_STORAGE = 102
+        const val FLAG_PERM_STORAGE = 99
+
+        //선택된 버튼 색상 관련 상수
+        const val SELECT_PURPLE_BUTTON = 10
+        const val SELECT_RED_BUTTON = 11
+        const val SELECT_YELLOW_BUTTON = 12
+    }
+}
