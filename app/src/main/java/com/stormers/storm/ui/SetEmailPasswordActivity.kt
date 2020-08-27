1eb50df7756dc33cdc4e18d1809ad6964e5ae52c
SONPYEONGHWA <ssrsph@naver.com>
2020-08-27 19:44:55 +0900

    add: 서비스 이용약관 xml 추가 및 텍스트와처 로직 수정

--- app/src/main/java/com/stormers/storm/ui/SetEmailPasswordActivity.kt
+++ app/src/main/java/com/stormers/storm/ui/SetEmailPasswordActivity.kt
@@ -6,6 +6,7 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.view.View
+import android.widget.TextView
 import com.stormers.storm.R
 import com.stormers.storm.SignUp.CheckEmailDuplicationModel
 import com.stormers.storm.SignUp.InterfaceSignUp
@@ -25,82 +26,23 @@ class SetEmailPasswordActivity : BaseActivity() {
 
     val buttonArray = ArrayList<StormDialogButton>()
 
+    var isValidEmail = false
+    var isValidPassword = false
+
+    var lastEmailCheck = false
+    var lastValidPassword = false
+    var lastValidPasswordCheck = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_set_email_password)
 
-        signUpTextWatcher()
+        checkPasswordTextWatcher()
         checkVaildEmailType()
         goSignUpActivity()
         goBackActivity()
-    }
 
-    fun signUpTextWatcher() {
-
-        edittext_input_password.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-
-                if (edittext_input_password.text!!.length < 8){
-                    textview_password_warning.visibility = View.VISIBLE
-                    textview_password_warning.setText(R.string.input_password_more_char)
-                    unusableNextButton()
-
-                } else {
-
-                    if (!edittext_input_password.text.toString().equals(edittext_password_check.text.toString())){
-                        textview_password_warning.setText(R.string.check_password)
-                        textview_password_warning.visibility = View.VISIBLE
-                        unusableNextButton()
-                    } else {
-                        textview_password_warning.visibility = View.GONE
-                        usableNextButton()
-                    }
-                }
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
-        })
-
-        edittext_password_check.addTextChangedListener(object  : TextWatcher{
-            override fun afterTextChanged(s: Editable?) {
-
-                if (edittext_password_check.text!!.length < 8){
-                    textview_password_warning.visibility = View.VISIBLE
-                    textview_password_warning.setText(R.string.input_password_more_char)
-                    unusableNextButton()
-
-                } else {
-                    if (!edittext_password_check.text.toString().equals(edittext_input_password.text.toString())){
-                        textview_password_warning.setText(R.string.check_password)
-                        textview_password_warning.visibility = View.VISIBLE
-                        unusableNextButton()
-                    } else {
-                        textview_password_warning.visibility = View.GONE
-                        usableNextButton()
-                    }
-
-                }
-
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            }
-        })
-
-        //전체 지우기 버튼 활성화/비활성화
-        edittext_input_email.setRemoveAllTextWatcher()
-        edittext_input_password.setRemoveAllTextWatcher()
-        edittext_password_check.setRemoveAllTextWatcher()
-
-    }
-
-    fun isEmailValid(email: String): Boolean {
-        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+        lastCheckNextButtonEnabled()
     }
 
     fun goBackActivity(){
@@ -108,8 +50,6 @@ class SetEmailPasswordActivity : BaseActivity() {
             startActivity(Intent(this, LoginActivity::class.java))
         }
     }
-
-
 
     fun goSignUpActivity(){
         button_next_signup.setOnClickListener{
@@ -161,20 +101,33 @@ class SetEmailPasswordActivity : BaseActivity() {
         }
     }
 
+    fun setRemoveAllTextWatcher() {
+        //전체 지우기 버튼 활성화/비활성화
+        edittext_input_email.setRemoveAllTextWatcher()
+        edittext_input_password.setRemoveAllTextWatcher()
+        edittext_password_check.setRemoveAllTextWatcher()
+    }
+
+    fun lastCheckNextButtonEnabled() {
+        if(lastEmailCheck == true && lastValidPassword == true && lastValidPasswordCheck == true && isCheckedLegacy() == true){
+            usableNextButton()
+        } else {
+            unusableNextButton()
+        }
+    }
+
     fun checkVaildEmailType() {
 
         edittext_input_email.addTextChangedListener(object : TextWatcher{
             override fun afterTextChanged(s: Editable?) {
+                isValidEmail = checkEmailCondition(edittext_input_email)
 
-                if(isEmailValid(edittext_input_email.text.toString()) == false){
-                    textview_email_warning.setText(R.string.check_email_type)
-                    textview_email_warning.visibility = View.VISIBLE
-                    unusableNextButton()
-
+                if( isValidEmail == true) {
+                    lastEmailCheck = true
                 } else {
-                    textview_email_warning.visibility = View.GONE
-                    usableNextButton()
+                    lastEmailCheck = false
                 }
+
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -183,6 +136,108 @@ class SetEmailPasswordActivity : BaseActivity() {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
             }
         })
+
+    }
+
+    fun checkPasswordTextWatcher() {
+
+        edittext_input_password.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+                isValidPassword = checkPasswordCondition(edittext_input_password, edittext_password_check, textview_password_warning)
+
+                if(isValidPassword == true) {
+                    lastValidPassword = true
+                } else {
+                    lastValidPassword = false
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
+        })
+
+        edittext_password_check.addTextChangedListener(object  : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+
+                isValidPassword = checkPasswordCondition(edittext_password_check, edittext_input_password, textview_password_warning)
+
+                if( isValidPassword == true) {
+                    lastValidPasswordCheck = true
+                } else {
+                    lastValidPasswordCheck = false
+                }
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
+
+        setRemoveAllTextWatcher()
+
+    }
+
+    fun checkPasswordCondition(editText1: StormEditText, editText2: StormEditText, textView: TextView) : Boolean {
+
+        if (editText1.text!!.length < 8){
+            textView.visibility = View.VISIBLE
+            textView.setText(R.string.input_password_more_char)
+
+
+            return false
+
+        } else {
+
+            if (!editText1.text.toString().equals(editText2.text.toString())){
+                textView.setText(R.string.check_password)
+                textView.visibility = View.VISIBLE
+
+                return false
+
+            } else {
+                textView.visibility = View.GONE
+
+                return true
+            }
+        }
+    }
+
+    fun checkEmailCondition(editText: StormEditText) : Boolean {
+
+        if(isEmailValid(editText.text.toString()) == false){
+            textview_email_warning.setText(R.string.check_email_type)
+            textview_email_warning.visibility = View.VISIBLE
+
+            return false
+
+        } else {
+            textview_email_warning.visibility = View.GONE
+
+            return true
+        }
+
+    }
+
+    fun isEmailValid(email: String): Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    fun isCheckedLegacy() : Boolean {
+
+        if(checkbox_service_lagacy.isChecked == true && checkbox_personal_information.isChecked == true){
+
+            return true
+
+        } else {
+
+            return false
+        }
+
     }
 
     private fun unusableNextButton(){
