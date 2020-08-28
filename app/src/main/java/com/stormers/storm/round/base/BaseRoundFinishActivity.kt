611ec195e6959012086373350428063d874c350e
김성규 <link5658@gmail.com>
2020-08-28 17:11:32 +0900

    fix: 라운드 나가기를 하면 해당 라운드가 조회되지 않던 문제 수정

--- app/src/main/java/com/stormers/storm/round/base/BaseRoundFinishActivity.kt
+++ app/src/main/java/com/stormers/storm/round/base/BaseRoundFinishActivity.kt
@@ -1,25 +1,17 @@
 package com.stormers.storm.round.base
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import com.stormers.storm.R
 import com.stormers.storm.card.fragment.RoundMeetingFragment
 import com.stormers.storm.customview.dialog.StormDialogBuilder
 import com.stormers.storm.customview.dialog.StormDialogButton
-import com.stormers.storm.network.RetrofitClient
-import com.stormers.storm.network.SimpleResponse
 import com.stormers.storm.network.SocketClient
-import com.stormers.storm.round.network.RequestRound
 import com.stormers.storm.ui.GlobalApplication
 import com.stormers.storm.ui.ParticipatedProjectDetailActivity
 import kotlinx.android.synthetic.main.activity_round_progress.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import java.lang.StringBuilder
 
 open class BaseRoundFinishActivity : BaseRoundProgressActivity() {
@@ -83,36 +75,13 @@ open class BaseRoundFinishActivity : BaseRoundProgressActivity() {
         finish()
     }
 
-    //라운드 나가기 통신
+    //라운드 나가기
     private fun exitRound() {
-        RetrofitClient.create(RequestRound::class.java).exitRound(GlobalApplication.userIdx,
-            GlobalApplication.currentProject!!.projectIdx, GlobalApplication.currentRound!!.roundIdx)
-            .enqueue(object : Callback<SimpleResponse> {
+        //소켓으로 나감을 알림
+        leaveSocket()
 
-                @SuppressLint("LongLogTag")
-                override fun onFailure(call: Call<SimpleResponse>, t: Throwable) {
-                    Log.d(TAG, "exitRound: Fail. ${t.message}")
-                }
-
-                @SuppressLint("LongLogTag")
-                override fun onResponse(call: Call<SimpleResponse>, response: Response<SimpleResponse>) {
-                    if (response.isSuccessful) {
-                        if (response.body()!!.success) {
-                            Log.d(TAG, "exitRound: Success.")
-
-                            //소켓으로 나감을 알림
-                            leaveSocket()
-
-                            //종료
-                            finish()
-                        } else {
-                            Log.d(TAG, "exitRound: Not success. ${response.body()!!.message}")
-                        }
-                    } else {
-                        Log.d(TAG, "exitRound: Not successful. ${response.message()}")
-                    }
-                }
-            })
+        //종료
+        finish()
     }
 
     private fun leaveSocket() {
