f3ea2763de5ebcbdcf91f46a32ef7a47f15d7458
KIM SEONGGYU <link5658@gmail.com>
2021-04-20 16:09:15 +0900

    feat: Add SplashFragment to SingleActivity

---
+++ app/src/main/java/com/stormers/storm/SingleActivity.kt
@@ -0,0 +1,12 @@
+package com.stormers.storm
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SingleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single)
+    }
+}
